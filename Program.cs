867171b811774700aab3a2ce314f3b7b21ab720d
123Framework <balatozar@gmail.com>
2026-04-02 17:55:46 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -15,9 +15,15 @@ builder.Services.AddScoped<IAIService, OpenRouterAiService>();
 builder.Services.Configure<OpenRouterOptions>(builder.Configuration.GetSection("OpenRouter"));
 builder.Services.AddHttpClient();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy => policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod());
+});
+
 
 
 var app = builder.Build();
+app.UseCors("AllowFrontend");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
